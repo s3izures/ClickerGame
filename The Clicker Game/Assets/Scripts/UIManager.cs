24ae1bf307874e627930cec27006e10b99722683
eN <seizuresthree@gmail.com>
2024-07-22 15:25:47 -0700

    Added shop mechanic, no items yet

--- The Clicker Game/Assets/Scripts/UIManager.cs
+++ The Clicker Game/Assets/Scripts/UIManager.cs
@@ -6,9 +6,26 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI pointsDisplay;
+    [SerializeField] CanvasGroup gro;
+    [SerializeField] GameObject shop;
+    [SerializeField] Animator anim;
 
     void Update()
     {
         pointsDisplay.text = GameManager.Instance.getPoints().ToString();
+    }
+
+    public void OpenShop()
+    {
+        shop.SetActive(true);
+
+        gro.alpha = 1.0f;
+        gro.interactable = true;
+        anim.Play("Shop Open");
+    }
+    public void CloseShop()
+    {
+        gro.interactable = false;
+        anim.Play("Shop Close");
     }
 }
